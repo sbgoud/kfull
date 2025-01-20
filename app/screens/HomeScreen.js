0ca0c8b56815e5b91b6aa99ebe8b94fd9f064205
Shashank Bandari <shashank2bandari@gmail.com>
2025-01-21 02:12:48 +0530

    added recent calls in home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 // app/screens/HomeScreen.js
 import React from 'react';
 import {
+  SafeAreaView,
   View,
   Text,
   StyleSheet,
@@ -10,6 +11,7 @@ import {
 } from 'react-native';
 import ProfileCard from '../components/ProfileCard';
 
+// Dummy data arrays
 const FEATURED_DATA = [
   {
     id: '101',
@@ -70,80 +72,114 @@ const ALL_DATA = [
     audioPrice: '$1.00/min',
     videoPrice: '$2.00/min',
   },
+];
+
+const RECENT_CALLS = [
   {
-    id: '205',
-    name: 'Lucas Rivera',
-    profession: 'Fitness Coach',
-    avatar: 'https://images.pexels.com/photos/1089166/pexels-photo-1089166.jpeg',
+    id: '301',
+    name: 'Chris Evans',
+    profession: 'Marketing Consultant',
+    avatar: 'https://images.pexels.com/photos/3777555/pexels-photo-3777555.jpeg',
     audioPrice: '$2.00/min',
     videoPrice: '$3.00/min',
+  },
+  {
+    id: '302',
+    name: 'Diana Prince',
+    profession: 'Life Coach',
+    avatar: 'https://images.pexels.com/photos/264039/pexels-photo-264039.jpeg',
+    audioPrice: '$1.80/min',
+    videoPrice: '$2.80/min',
   },
 ];
 
 export default function HomeScreen({ route }) {
-  // Email from login (dummy data for now)
   const userEmail = route.params?.userEmail || 'GuestUser@example.com';
 
   return (
-    <ScrollView style={styles.scrollContainer}>
-      <View style={styles.container}>
-        {/* App Logo + Name */}
-        <View style={styles.headerContainer}>
-          <Image 
-            source={require('../assets/small.jpg')} 
-            style={styles.logo} 
-            resizeMode="contain"
+    <SafeAreaView style={styles.safeArea}>
+      {/* Wrap screen in ScrollView for vertical content */}
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.container}>
+          {/* Header: Logo + App Name */}
+          <View style={styles.headerContainer}>
+            <Image
+              source={require('../assets/small.jpg')}
+              style={styles.logo}
+              resizeMode="contain"
+            />
+            <Text style={styles.appTitle}>KENNERKALL</Text>
+          </View>
+
+          {/* Welcome text */}
+          <Text style={styles.welcomeText}>Welcome, {userEmail}!</Text>
+
+          {/* Featured Professionals (Horizontal Slider) */}
+          <Text style={styles.sectionTitle}>Featured Professionals</Text>
+          <FlatList
+            data={FEATURED_DATA}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <ProfileCard
+                name={item.name}
+                profession={item.profession}
+                avatar={item.avatar}
+                audioPrice={item.audioPrice}
+                videoPrice={item.videoPrice}
+              />
+            )}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.horizontalListContainer}
+            // Add spacing between items
+            ItemSeparatorComponent={() => <View style={{ width: 16 }} />}
           />
-          <Text style={styles.appTitle}>KENNERKALL</Text>
+
+          {/* All Professionals (Vertical List) */}
+          <Text style={styles.sectionTitle}>All Professionals</Text>
+          <FlatList
+            data={ALL_DATA}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <ProfileCard
+                name={item.name}
+                profession={item.profession}
+                avatar={item.avatar}
+                audioPrice={item.audioPrice}
+                videoPrice={item.videoPrice}
+              />
+            )}
+            // Turn off scrolling so it doesn't conflict with outer ScrollView
+            scrollEnabled={false}
+          />
+
+          {/* Recent Calls (Vertical List) */}
+          <Text style={styles.sectionTitle}>Recent Calls</Text>
+          <FlatList
+            data={RECENT_CALLS}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <ProfileCard
+                name={item.name}
+                profession={item.profession}
+                avatar={item.avatar}
+                audioPrice={item.audioPrice}
+                videoPrice={item.videoPrice}
+              />
+            )}
+            scrollEnabled={false}
+          />
         </View>
-
-        {/* Welcome text */}
-        <Text style={styles.welcomeText}>Welcome, {userEmail}!</Text>
-
-        {/* Featured Professionals (Horizontal) */}
-        <Text style={styles.sectionTitle}>Featured Professionals</Text>
-        <FlatList
-          data={FEATURED_DATA}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <ProfileCard
-              name={item.name}
-              profession={item.profession}
-              avatar={item.avatar}
-              audioPrice={item.audioPrice}
-              videoPrice={item.videoPrice}
-            />
-          )}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.horizontalListContainer}
-        />
-
-        {/* All Professionals (Vertical) */}
-        <Text style={styles.sectionTitle}>All Professionals</Text>
-        <FlatList
-          data={ALL_DATA}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <ProfileCard
-              name={item.name}
-              profession={item.profession}
-              avatar={item.avatar}
-              audioPrice={item.audioPrice}
-              videoPrice={item.videoPrice}
-            />
-          )}
-          // We want a vertical list. 
-          // Because we wrapped entire screen in a ScrollView, 
-          // we can disable scroll in the FlatList to avoid conflict.
-          scrollEnabled={false}
-        />
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
   scrollContainer: {
     flex: 1,
     backgroundColor: '#fff',
@@ -157,8 +193,8 @@ const styles = StyleSheet.create({
     marginBottom: 12,
   },
   logo: {
-    width: 60,
-    height: 60,
+    width: 50,
+    height: 50,
     marginRight: 10,
   },
   appTitle: {
@@ -175,9 +211,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '600',
     marginBottom: 8,
+    marginTop: 16,
     color: '#374151',
   },
   horizontalListContainer: {
-    paddingBottom: 16, // spacing for horizontal slider
+    paddingBottom: 16,
   },
 });
